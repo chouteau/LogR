2507dae1ad9bb13015c984ad75c4ea1437313a86
Marc Chouteau <marc_chouteau@hotmail.com>
2022-04-14 18:13:43 +0200

    WebDesign

--- src/ConsoleClient/Program.cs
+++ src/ConsoleClient/Program.cs
@@ -12,28 +12,43 @@ builder.Services.AddLogRPush(logSettings =>
     logSettings.LogServerUrlList.Add("http://localhost:5276/");
 });
 
+builder.Services.AddSingleton<VerryVerrySuperLongVerryVerrySuperLongVerryVerrySuperLongVerryVerrySuperLong>();
+
 var app = builder.Build();
 
 app.Services.UseLogRPush();
 
 await app.StartAsync();
 
-var logger = app.Services.GetService<ILogger<Program>>();
+var c = app.Services.GetRequiredService<VerryVerrySuperLongVerryVerrySuperLongVerryVerrySuperLongVerryVerrySuperLong>();
 
-while (true)
-{
-    await Task.Delay(3 * 1000);
-	logger.LogCritical(new Exception("Critical1\nCritical2\n"), "Critical : {0}", DateTime.Now);
-	var error = new Exception(System.Environment.StackTrace);
-	error.Data.Add("Param1", "Value1");
-	error.Data.Add("Param2", "Value2");
-	logger.LogError(error, "Error : {Now}", DateTime.Now);
-	logger.LogWarning("Warning : {0}", DateTime.Now);
-	logger.LogInformation("Info : {0}", DateTime.Now);
-	logger.LogDebug("Debug : {0}", DateTime.Now);
-	logger.LogTrace("Trace : {0}", DateTime.Now);
-}
+await c.WriteLogs();
 
 Console.ReadKey();
 
+public class VerryVerrySuperLongVerryVerrySuperLongVerryVerrySuperLongVerryVerrySuperLong
+{
+	private ILogger _logger;
+	public VerryVerrySuperLongVerryVerrySuperLongVerryVerrySuperLongVerryVerrySuperLong(ILogger<VerryVerrySuperLongVerryVerrySuperLongVerryVerrySuperLongVerryVerrySuperLong> logger)
+	{
+		_logger = logger;
+	}
 
+	public async Task WriteLogs()
+	{
+		while (true)
+		{
+			await Task.Delay(3 * 1000);
+			_logger.LogCritical(new Exception("Critical1\nCritical2\n"), "Critical : {0}", DateTime.Now);
+			var error = new Exception(System.Environment.StackTrace);
+			error.Data.Add("Param1", "Value1");
+			error.Data.Add("Param2", "Value2");
+			_logger.LogError(error, "Error : {Now}", DateTime.Now);
+			_logger.LogWarning("Warning : {0}", DateTime.Now);
+			_logger.LogInformation("Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+			_logger.LogDebug("Debug : {0}", DateTime.Now);
+			_logger.LogTrace("Trace : {0}", DateTime.Now);
+		}
+
+	}
+}
