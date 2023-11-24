ad079b0a6fed88b23983b57ff6d240935e1df7ac
Marc Chouteau <marc_chouteau@hotmail.com>
2023-11-24 10:58:01 +0100

    Hack for insert js script into component

---
+++ src/LogRWebMonitor/Components/Monitor.razor.js
@@ -0,0 +1,8 @@
+﻿window.blazorExtensions = {
+	WriteLocalStorage: function (key, value) {
+		window.localStorage.setItem(key, value);
+	},
+	ReadLocalStorage: function (key) {
+			return window.localStorage.getItem(key);
+	}
+};
